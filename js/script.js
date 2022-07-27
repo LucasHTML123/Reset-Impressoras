c6dbe6cef9b7f65206540de75205b5cd9138eee4
tucaskohlermfa@gmail.com <tucaskohlermfa.com>
2022-07-27 08:34:01 -0300

    Finalização do Css e ínicio do JS

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function passoAPasso() {
+    const modeloInput = document.querySelector('#modelo').value;
+    const ol = document.querySelector('#steps');
+    
+    console.log(modeloInput, ol)
+}
