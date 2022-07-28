8df2718ae806cef48c7f0f1750de97521e9229c1
tucaskohlermfa@gmail.com <tucaskohlermfa.com>
2022-07-28 12:08:54 -0300

    CSS feito e ínicio do JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-function passoAPasso() {
-    const modeloInput = document.querySelector('#modelo').value;
-    const ol = document.querySelector('#steps');
-    
-    console.log(modeloInput, ol)
-}
+const ul1 = document.querySelector('#first-list');
+const ul2 = document.querySelector('#second-list');
+
+let lis1 = ul1.querySelectorAll('li');
+let lis2 = ul2.querySelectorAll('li');
+
+let reset = document.querySelector('#reset');
+
+lis1[0].addEventListener('click', function() {
+    reset.style.display = 'block';
+    reset.innerText = ' 1º Desligue o cabo fonte. \n';
+    reset.innerText += '2º Aperte e segure os botões: power cancel por 5 segundos. \n';
+    reset.innerText += '3º Re-ligue a impressora.'
+})
